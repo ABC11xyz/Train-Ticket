13ec9dde44b699535babebd22a9df25aee27b95a
ABC11xyz <ruvishushukla1@gmail.com>
2025-02-19 16:12:35 +0530

    ways

--- Backend/index.js
+++ Backend/index.js
@@ -11,13 +11,13 @@ const app = express();
 
 const PORT = process.env.PORT || 5100;
 
-const corsOptions = {
-  origin: [ "http://localhost:5173"], 
-  methods: ['GET', 'POST'], 
-  credentials: true,
-};
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
-app.use(cors(corsOptions)); 
 app.use(express.json());
 
 
