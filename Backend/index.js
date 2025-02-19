d03c172ef681381a6b2961cc6da51ede08407876
ABC11xyz <ruvishushukla1@gmail.com>
2025-02-19 16:17:18 +0530

    headers

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import dotenv from 'dotenv'
 import connectDB from './DB/dbConnect.js';
 import TrainRoutes from './Routes/TrainRoutes.js'
-import cors from 'cors'
 
 
 dotenv.config();
@@ -12,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5100;
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Origin', 'https://train-ticket-rvn.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
